d62e2372fec13073bf7f240ce912056a9851ef60
AngelmoranT <dam.amorant@institutoserpis.org>
2017-03-02 13:07:56 +0100

    Modificacion : solución básica

--- CGtk/CGtk/MainWindow.cs
+++ CGtk/CGtk/MainWindow.cs
@@ -1,41 +1,55 @@
 using System;
+using System.Diagnostics;
 using Gtk;
+using System.Collections.Generic;
+using CGtk;
+
 
 public partial class MainWindow: Gtk.Window
-{	
+{    
 	public MainWindow (): base (Gtk.WindowType.Toplevel)
 	{
 		Build ();
-		labelSaludo.Text = "Hola. Pon tu nombre y pulsa Adelante.";
 
-		for (int vez = 0; vez < 5; vez++) {
+		Table table = new Table (9, 10, true);
+
+		IList<int> bolas = new List<int> ();
+		IList<Button> buttons = new List<Button> ();
+		for (int numero = 1; numero <= 90; numero ++) {
+			bolas.Add (numero);
+			uint index = (uint)numero - 1;
 			Button button = new Button ();
-			button.Label = "" + vez;
+			button.Label = "" + numero;
 			button.Visible = true;
-			vBoxMain.Add (button);
-			button.Clicked += OnButtonGoForwardClicked;
+			uint row = index / 10;
+			uint column = index % 10;
+			table.Attach (button, column, column + 1, row, row + 1);
+			buttons.Add (button);
 		}
+		table.Visible = true;
+		vBoxMain.Add (table);
+
+		Random random = new Random ();
+		buttonGoForward.Clicked += delegate {
+			int randomIndex = random.Next(bolas.Count);
+			int bola = bolas[randomIndex];
+			Console.WriteLine ("bola =" + bola);
+			bolas.RemoveAt(randomIndex);
+			if (bolas.Count == 0)
+				buttonGoForward.Sensitive = false;
+
+			Process.Start("espeak", " -v es " + EspeakHelper.ToEspeak(bola));
+			buttons[bola - 1].ModifyBg(StateType.Normal, new Gdk.Color(0, 255, 0));
+		};
 
 	}
 
 	protected void OnDeleteEvent (object sender, DeleteEventArgs a)
 	{
-		Console.WriteLine ("OnDeleteEvent");
 		Application.Quit ();
 		a.RetVal = true;
 	}
-
-
-	protected void OnButtonGoForwardClicked (object sender, EventArgs e)
-	{
-		Button button = (Button)sender;
-		labelSaludo.Text = "Hola " + entryNombre.Text + "pulsado" + button.Label;	
-	}
 }
-
-
-//Console.WriteLine("27<</10 = " + 27/10 + "resto = " + 20%7);
-
 
 
 
